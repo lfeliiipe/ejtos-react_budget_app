3472cc167d4276c3f0ba4cf0a0006510f19d8a79
lfeliiipe <53595200+lfeliiipe@users.noreply.github.com>
2023-10-24 01:15:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,19 +19,32 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            //ExpenseList component
+                            <div className='my-5'>
+                                <h1>Allocation</h1>
+                                <ExpenseList />
+                            </div>
                         }         
 
                         {
@@ -36,7 +52,11 @@ const App = () => {
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                            //AllocationForm component
+                            <div>
+                                <h1>Change Allocation</h1>
+                                <AllocationForm />
+                            </div>
                         }        
 
                 </div>
