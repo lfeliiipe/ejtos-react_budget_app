ad0effd7eb144336119598b08b14a276a613df18
lfeliiipe <53595200+lfeliiipe@users.noreply.github.com>
2023-10-27 03:03:03 -0300

    Update DropdownCurrency.js

--- src/components/DropdownCurrency.js
+++ src/components/DropdownCurrency.js
@@ -4,19 +4,28 @@ import { AppContext } from '../context/AppContext';
 const DropdownCurrency = () => {
     const { dispatch, currency } = useContext(AppContext);
 
-    const [ newCurrency, setNewCurrency ] = useState('');
+    const [ newCurrency, setNewCurrency ] = useState(currency);
+
+    const changeCurrency = (event) => {
+        setNewCurrency(event.target.value);
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value,
+        });
+    };
 
     return (
-        <div className='input-group'>
-            <span className='input-group-text'>{currency}</span>
-            <button className='btn btn-secondary dropdown-toggle' type="button" data-bs-toggle="dropdown">Currency</button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item">$ Dollar</a></li>
-                <li><a class="dropdown-item">£ Pound</a></li>
-                <li><a class="dropdown-item">€ Euro</a></li>
-                <li><a class="dropdown-item">₹ Ruppee</a></li>
-            </ul>
+        <div className='alert alert-success'>
+            <label>Currency</label>
+            <select className="form-select" value={newCurrency} onChange={changeCurrency}>
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Ruppee</option>
+            </select>
+
         </div>
+        
     );
 };
 export default DropdownCurrency;
